84356df5b4fd2e86e1c4d91c7fc8f92b7d27db5b
ali574596 <numairh0@gmail.com>
2023-09-02 19:43:19 +0500

    adding comment

--- django_todos/settings.py
+++ django_todos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+# this is a comment
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
